4209598afaafe852e8fec9258f829d5d4fe0d954
Karl Stevens <karl.e.stevens@gmail.com>
2021-03-01 21:09:47 -0600

    Left Rotation but with int[]

--- Left Rotation/src/Solution.java
+++ Left Rotation/src/Solution.java
@@ -9,8 +9,24 @@ class Result {
 		List<Integer> result = new ArrayList<Integer>();
 		result.addAll(arr.subList(d, arr.size()));
 		result.addAll(arr.subList(0, d));
+		int[] irr = new int[arr.size()];
+		int size = irr.length;
 		return result;
 	}
+    static int[] rotLeft(int[] a, int d) {
+        int[] result = new int[a.length];
+        // loop is executed number of times from d to end of array
+        // copy source starting at d to destination starting at 0
+        for (int i=0; i<a.length-d; i++) {
+            result[i] = a[i+d];
+        }
+        // loop is executed number of times from start to d
+        // copy source starting at 0 to destination starting at length-d
+        for (int i=0; i<d; i++) {
+            result[a.length-d+i] = a[i];
+        }
+        return result;
+    }
 }
 
 public class Solution {
@@ -26,6 +42,11 @@ public class Solution {
         }
         List<Integer> result = Result.rotateLeft(d, arr);
         System.out.println(result);
+
+        int[] nlist = new int[] {1, 2, 3, 4, 5};
+        int[] rlist = Result.rotLeft(nlist, d);
+        for (int i : rlist)
+        	System.out.print(i + " ");
         scan.close();
     }
 }
