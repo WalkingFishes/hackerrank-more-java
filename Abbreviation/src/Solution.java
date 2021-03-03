d889d6d4f8919d237cb9e8c63a415b559b779991
Karl Stevens <karl.e.stevens@gmail.com>
2021-03-03 12:10:48 -0600

    Abbreviation challenge - 7 of 16 test cases passed

--- Abbreviation/src/Solution.java
+++ Abbreviation/src/Solution.java
@@ -6,18 +6,25 @@ public class Solution {
 
 	static String abbreviation(String a, String b) {
 		// Append all chars from a that are in b to a StringBuffer
+		// Go through character by character comparison of a and b
+		// If a is upper case, add to StringBuffer s
+		// If a is lowercase, compare to b at current b index
+		//     If match (upper or lower) add to s
+		//     If no match (include if end of b is reached), do not add to s
+		// At end of a, compare b to s
 		StringBuffer s = new StringBuffer(a.length());
-		for (int i=0; i<a.length(); i++) {
+		int j=0; // index for String b
+		for (int i=0; i<a.length(); i++) { // index for String a
 			// Only lowercase letters can be deleted
 			// If upper case, copy it
-			// otherwise, copy it if it exists in b
+			// otherwise, copy it if it exists in b at current b index
 			char c = a.charAt(i);
 			if (Character.isUpperCase(c)) {
 				s.append(c);
-			} else if (s.length() < b.length()) {
-				if (b.indexOf(Character.toUpperCase(c)) >= 0) {
-					s.append(Character.toUpperCase(c));
-				}
+				j++;
+			} else if (j < b.length() && b.charAt(j) == Character.toUpperCase(c)) { // a is lowercase
+				s.append(Character.toUpperCase(c));
+				j++;
 			}
 		}
 //		System.out.println(s);
