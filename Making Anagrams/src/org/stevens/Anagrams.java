9c12b101529f7bc9624ffd737ce659511060ab0a
Karl Stevens <karl.e.stevens@gmail.com>
2021-03-30 14:31:01 -0500

    Strings - Making Anagrams - passes all tests

--- Making Anagrams/src/org/stevens/Anagrams.java
+++ Making Anagrams/src/org/stevens/Anagrams.java
@@ -4,6 +4,8 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Scanner;
@@ -12,47 +14,32 @@ public class Anagrams {
     
     static int makeAnagram(String a, String b) {
         /*
-         * Remove all letters in a that aren't in b and count the removals
-         * Remove all letters in b that aren't in a and count the removals
-         * Take difference in string lengths
-         * Sum the 3 counts to equal the number of deletions to make the two string anagrams.
-         * 
-         */
-        /* Convert a and b to HashSets (makes unique chars)
-         * 
-         * 
-         */
-        HashSet<String> hashA = new HashSet<String>(Arrays.asList(a.split("")));
-//        System.out.println("A: " + hashA);
-        HashSet<String> hashB = new HashSet<String>(Arrays.asList(b.split("")));
-//        System.out.println("B: " + hashB);
-        
-        // aNotInB contains all the chars in a that aren't in b
-        HashSet<String> aNotInB = new HashSet<String>();
-        aNotInB = (HashSet<String>) hashA.clone();
-        aNotInB.removeAll(hashB);
-//        System.out.println("A not in B: " + aNotInB);
-        // aList contains all the chars in a that are in b
-        List<String> aList = new ArrayList<String>(Arrays.asList(a.split("")));
-        aList.removeAll(aNotInB);
-//        System.out.println("A in B: " + aList);
+         * Convert Strings A and B to sorted Lists
+         * Walk through shortest list and record number of chars to keep
+         * Number of total deletions is length of A - (keep count) + length of B - (keep count)
+         */ 
 
-        // bNotInA contains all the chars in b that aren't in a
-        HashSet<String> bNotInA = new HashSet<String>();
-        bNotInA = (HashSet<String>) hashB.clone();
-        bNotInA.removeAll(hashA);
-//        System.out.println("B not in A: " + bNotInA);
-        // bList contains all the chars in b that are in a
+        // Convert a and b to sorted Lists
+        List<String> aList = new ArrayList<String>(Arrays.asList(a.split("")));
+        aList.sort(Comparator.comparing(String::toString));
+        System.out.println("A sorted: " + aList);
         List<String> bList = new ArrayList<String>(Arrays.asList(b.split("")));
-        bList.removeAll(bNotInA);
-//        System.out.println("B in A: " + bList);
-        
-        // No. chars deleted from A
-        int delA = a.length() - aList.size();
-        // No. chars deleted from B
-        int delB = a.length() - bList.size();
-        
-        return delA + delB;
+        bList.sort(Comparator.comparing(String::toString));
+        System.out.println("B sorted: " + bList);
+
+        // Get unique list of chars in A
+        HashSet<String> hashA = new HashSet<String>(Arrays.asList(a.split("")));
+        System.out.println("A: " + hashA);
+
+        int keep = 0; // This is the length of the anagram
+        for (String s : hashA) {
+            // keep the lowest value between cntA and cntB
+            int cntA = Collections.frequency(aList, s);
+            int cntB = Collections.frequency(bList, s);
+            keep += (cntA > cntB) ? cntB : cntA;
+        }
+        return a.length() + b.length() - 2*keep;
+
     }
 
     public static void main(String[] args) throws FileNotFoundException {
