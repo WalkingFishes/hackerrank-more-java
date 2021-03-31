ade4e4bae4b87ff38061131aab572e1b05e36919
Karl Stevens <karl.e.stevens@gmail.com>
2021-03-31 13:24:58 -0500

    Reverse String - Reversing words

--- Reverse String/src/org/stevens/Reverse.java
+++ Reverse String/src/org/stevens/Reverse.java
@@ -4,6 +4,8 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.time.Duration;
 import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Reverse {
@@ -28,6 +30,20 @@ public class Reverse {
         return s.substring(s.length()-1) + reverseString(s.substring(0, s.length()-1));
 //        return Character.toString(s.charAt(s.length()-1)) + reverseString(s.substring(0, s.length()-1));
     }
+    
+    static String reverseWords(String s) {
+        /*
+         * Split string (on spaces) into words, add to Array
+         * Iterate backwards through Array and concatenate elements with space in between
+         */
+        String[] arr = s.split(" ");
+        String newString = arr[arr.length-1];
+        for (int i=arr.length-2; i>=0; i--) {
+            newString = newString + " " + arr[i];
+        }
+        return newString;
+        
+    }
 
     public static void main(String[] args) throws FileNotFoundException {
         File file = new File("src/input.txt");
@@ -49,6 +65,8 @@ public class Reverse {
         rev = reverseString2(s);
         end = Instant.now();
         System.out.println("reverseString2: " + rev + " " + Duration.between(start, end));
+
+        System.out.println(reverseWords(s));
 
         scan.close();
 
